2981ecdf83d2994f9a1d8079a57d38e9c6ed3d66
Artiom Net <neartiom@yande.ru>
2024-09-10 20:03:08 +0400

    correction

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,14 @@ public class Main {
         int score = 100; // Начальный счет
         int coming; // Пополнение счета
         int bonus; // Количество бонусов
-        {
-            coming = 300;
-            if (coming > 1000) {
-                bonus = (coming / 100);
-                System.out.println("Бонусов начислено: " + (bonus));
-                System.out.println("На счету: " + (score + coming + bonus));
-            } else {
-                System.out.println("На счету: " + (score + coming));
-            }
+
+        coming = 1100;
+        if (coming > 1000) {
+            bonus = (coming / 100);
+            System.out.println("Бонусов начислено: " + (bonus));
+            System.out.println("На счету: " + (score + coming + bonus));
+        } else {
+            System.out.println("На счету: " + (score + coming));
         }
-        {
-            coming = 1100;
-            if (coming > 1000) {
-                bonus = (coming / 100);
-                System.out.println("Бонусов начислено: " + (bonus));
-                System.out.println("На счету: " + (score + coming + bonus));
-            } else {
-                System.out.println("На счету: " + (score + coming));
-            }
-        }
-
-
-
     }
 }
